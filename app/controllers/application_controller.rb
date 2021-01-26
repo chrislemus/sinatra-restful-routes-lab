8926b0b37eb10bb454f79aa5ddfae3e97307449f
chrislemus <icrislemus@gmail.com>
2021-01-25 21:25:24 -0500

    commit tree a885a15027d4a43a9842a79363630a27e86232a3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,42 @@ class ApplicationController < Sinatra::Base
 
   # code actions here!
 
+  get('/recipes') {
+    @recipes = Recipe.all
+    erb :index
+  }
+
+  post('/recipes') {
+    @recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+    redirect "/recipes/#{Recipe.last.id}"
+  }
+
+  get('/recipes/new') {
+    erb :new
+  }
+
+  get('/recipes/:id/edit') {
+    @recipe = Recipe.find(params[:id])
+    erb :edit
+  }
+
+  patch('/recipes/:id/edit') {
+    recipe = Recipe.find(params[:id])
+    recipe.name = params[:name]
+    recipe.ingredients = params[:ingredients]
+    recipe.cook_time = params[:cook_time]
+    recipe.save
+    redirect "/recipes/#{recipe.id}"
+  }
+
+  get('/recipes/:id') {
+    @recipe = Recipe.find(params[:id])
+    erb :show
+  }
+
+  delete('/recipes/:id') {
+    recipe = Recipe.find(params[:id])
+    recipe.destroy
+  }
+
 end
